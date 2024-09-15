50ed9bde0d59e4982fd32ddf440bac15f9303a27
Sushant kumar <ksushant6566@gmail.com>
2024-09-15 21:56:13 +0530

    feat: add basic auth screen before homepage

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 import { getServerAuthSession } from '~/server/auth'
+import { ModeToggle } from './theme-toggle'
 
 export const Header = async () => {
   const session = await getServerAuthSession()
 
   return (
-    <div className="flex w-full items-center gap-4">
+    <div className="flex w-full items-center justify-between gap-4 pr-8">
+      <div className='flex items-center gap-4 w-full'>
       <div className="relative ml-8 flex items-center">
         <div className="absolute left-0 top-0 flex h-11 w-11 -translate-x-[78%] -translate-y-[6%] items-center justify-center rounded-full bg-blue-800 cursor-pointer">
           <a href="https://mindtab.in" target="_blank">
@@ -33,17 +35,10 @@ export const Header = async () => {
           <p className="text-sm font-medium">2 days streak</p>
         </div>
       </div>
-      {/* <div className="flex items-center gap-4">
-        {!session && (
-          <Link
-            href={session ? '/api/auth/signout' : '/api/auth/signin'}
-            className="rounded-full bg-white/10 px-6 py-2 text-sm font-medium no-underline transition hover:bg-white/20"
-          >
-            {session ? 'Sign out' : 'Sign in'}
-          </Link>
-        )}
+      </div>
+        <div>
         <ModeToggle />
-      </div> */}
+        </div>
     </div>
   )
 }
--- src/app/_components/home.tsx
+++ src/app/_components/home.tsx
@@ -3,8 +3,18 @@ import { Goals } from './goals'
 import { Habits } from './habits'
 import { Header } from './header'
 import { Journals } from './jounal'
+import { getServerAuthSession } from '~/server/auth'
+import Auth from './auth'
 
 export default async function Component() {
+
+  const session = await getServerAuthSession()
+  const user = session?.user
+
+  if(!user) return (
+    <Auth />
+  )
+
   return (
     <div className="h-screen w-screen max-w-screen-2xl space-y-8 p-20 px-28">
       <div className="grid grid-cols-10 gap-0">
