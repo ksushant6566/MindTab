48e56f6b7bd7fb2ea049b3de8df09e170e970149
Sushant kumar <ksushant6566@gmail.com>
2024-09-29 07:23:38 +0530

    feat: enable optimistic updates on habit tracker

--- src/app/_components/habits.tsx
+++ src/app/_components/habits.tsx
@@ -6,7 +6,9 @@ import { HabitTable } from './habit-table'
 
 export const Habits: React.FC = () => {
 
-const { data: habits, refetch: refetchHabits } = api.habits.getAll.useQuery()
+  const apiUtils = api.useUtils()
+
+  const { data: habits, refetch: refetchHabits } = api.habits.getAll.useQuery()
   const {
     mutate: deleteHabit,
     isPending: isDeletingHabit,
@@ -32,14 +34,64 @@ const { data: habits, refetch: refetchHabits } = api.habits.getAll.useQuery()
   const { data: habitTracker, refetch: refetchHabitTracker } = api.habitTracker.getAll.useQuery()
 
   const { mutate: trackHabit } = api.habits.trackHabit.useMutation({
-    onSuccess: () => {
-      refetchHabitTracker()
+    async onMutate(variables) {
+      // cancel any existing requests to avoid conflicts
+      await apiUtils.habitTracker.getAll.cancel()
+
+      // store the previous value of habitTracker
+      const previousHabitTracker = apiUtils.habitTracker.getAll.getData() ?? []
+
+      // set the new value of habitTracker
+      apiUtils.habitTracker.getAll.setData(undefined, [
+        ...previousHabitTracker,
+        {
+          habitId: variables.habitId,
+          // return date in the format YYYY-MM-DD
+          date: variables.date.toISOString().split('T')[0]!,
+          status: "completed",
+          id: "1",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: "1",
+        }
+      ])
+
+      // return the previous value of habitTracker, this is used to revert the mutation in case of an error
+      return { previousHabitTracker }
+    },
+
+    onError(error, variables, context) {
+      apiUtils.habitTracker.getAll.setData(undefined, context?.previousHabitTracker ?? [])
+    },
+
+    onSettled() {
+      apiUtils.habitTracker.getAll.invalidate()
     },
   })
 
   const { mutate: untrackHabit } = api.habits.untrackHabit.useMutation({
-    onSuccess: () => {
-      refetchHabitTracker()
+    async onMutate(variables) {
+      // cancel any existing requests to avoid conflicts
+      await apiUtils.habitTracker.getAll.cancel()
+
+      // store the previous value of habitTracker
+      const previousHabitTracker = apiUtils.habitTracker.getAll.getData() ?? []
+
+      // set the new value of habitTracker
+      apiUtils.habitTracker.getAll.setData(undefined,
+        previousHabitTracker.filter(habit => !(habit.habitId === variables.habitId && habit.date === variables.date.toISOString().split('T')[0]!))
+      )
+
+      // return the previous value of habitTracker, this is used to revert the mutation in case of an error
+      return { previousHabitTracker }
+    },
+
+    onError(error, variables, context) {
+      apiUtils.habitTracker.getAll.setData(undefined, context?.previousHabitTracker ?? [])
+    },
+
+    onSettled() {
+      apiUtils.habitTracker.getAll.invalidate()
     },
   })
 
